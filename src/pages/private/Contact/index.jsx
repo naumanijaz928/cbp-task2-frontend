e3a1deaf0b816447ef0b2dc48ba7ad574c406c92
Muhammad Abdullah Aslam <abdullah201897@gmail.com>
2024-04-25 12:16:18 +0500

    Course pages populated with the list of modules

--- src/pages/private/Contact/index.jsx
+++ src/pages/private/Contact/index.jsx
@@ -1,8 +1,6 @@
-import { Typography } from "antd";
 import ContactUsForm from "../../../components/forms/contactusForm";
 import "./contact.scss";
 
-const { Title } = Typography;
 const Contact = () => {
   return (
     <div id="registerPage">
